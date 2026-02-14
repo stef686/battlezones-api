36f26ded1eb3f5831931ab7826d36bd3cc82e7c5
Stefan Ashwell <stef@stefashwell.com>
2026-02-14 23:10:55 +0000

    chore: initial system setup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,20 @@
 
 namespace Tests;
 
+use App\Models\User;
+use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use LazilyRefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $auth = User::factory()->create(['email' => 'stef@stefashwell.com']);
+
+        $this->actingAs($auth);
+    }
 }
